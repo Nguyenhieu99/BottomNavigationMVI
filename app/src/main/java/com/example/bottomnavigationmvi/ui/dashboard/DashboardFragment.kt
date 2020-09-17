906086f60462b5e1257c267e5a9b0e37b3e0dc6f
HieuNguyen <manhhiu0509@gmail.com>
2020-09-17 15:36:33 +0700

    change viewmodel lateinit

--- app/src/main/java/com/example/bottomnavigationmvi/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/bottomnavigationmvi/ui/dashboard/DashboardFragment.kt
@@ -6,13 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.bottomnavigationmvi.R
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+//    private lateinit var dashboardViewModel: DashboardViewModel
+    
+    private val dashboardViewModel: DashboardViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -20,8 +22,8 @@ class DashboardFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+//        dashboardViewModel =
+//                ViewModelProviders.of(this).get(DashboardViewModel::class.java)
 
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
